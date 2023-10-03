4cf76e3eb288333a16a9309fb7d6cd470bab20d7
MunyinyaMaurice <munyinya13@gmail.com>
2023-10-03 17:14:04 +0200

    create image function

--- app/Http/Controllers/RCHAcontroller/imagesController.php
+++ app/Http/Controllers/RCHAcontroller/imagesController.php
@@ -2,10 +2,27 @@
 
 namespace App\Http\Controllers\RCHAcontroller;
 
-use App\Http\Controllers\Controller;
+use App\Models\Image;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class imagesController extends Controller
 {
+    public function createImage(Request $request)
+    {
+        $request->validate([
+            'place_id' => 'required|exists:place,place_id',
+            'image_path' => 'required|string', 
+        ]);
+
+        $image = Image::create($request->all());
+
+        return response()->json([
+            'message' => 'Image created successfully',
+            'image' => $image
+        ], 201);
+    }
+
+   
     
 }
