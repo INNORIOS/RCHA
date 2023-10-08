182279318f72f95a82dc1eab2fd95ca9e2cce324
MunyinyaMaurice <munyinya13@gmail.com>
2023-10-08 02:56:10 +0200

    modifing Token model by adding   timestamp  field

--- app/Models/Token.php
+++ app/Models/Token.php
@@ -12,6 +12,7 @@ class Token extends Model
     protected $fillable = [
         'paid_token',
         'token_expires_at',
+        'timestamp',
     ];
     protected $guarded = [];
     public function payment()
