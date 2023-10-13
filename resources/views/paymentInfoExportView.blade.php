3f4d3be9c9017a9f17c6fde4372e4ed7ca35a548
MunyinyaMaurice <munyinya13@gmail.com>
2023-10-13 16:12:16 +0200

    creating paymentInfoExportView to allow download

---
+++ resources/views/paymentInfoExportView.blade.php
@@ -0,0 +1,14 @@
+@extends('layout-session')
+@section('tile','paymentInfoExport')
+@section('content')
+
+<div>
+    <h1>Download Test</h1>
+
+    <form action="http://127.0.0.1:8000/api/auth/exportPaymentInfoExcel" method="GET">
+        <button type="submit">Download Excel File</button>
+    </form>
+</div>
+
+
+@endsection
