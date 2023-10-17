a10dbd49ac29931eaa54eeabbd9b033bf293f9c4
MunyinyaMaurice <munyinya13@gmail.com>
2023-10-18 00:25:12 +0200

    function of storing category

--- app/Http/Controllers/RCHAcontroller/categoryController.php
+++ app/Http/Controllers/RCHAcontroller/categoryController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class categoryController extends Controller
 {
-    //
+    public function store(Request $request)
+{
+    $request->validate([
+        'category_name' => 'required|max:255',
+        'category_description' => 'nullable',
+    ]);
+
+    Category::create($request->all());
+
+    return redirect()->route('categories.index')->with('success', 'Category created successfully');
+}
+
 }
